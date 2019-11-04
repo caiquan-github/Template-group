bfd6f79970cb4025a5e4ad17c6c3b8ba0af6bae8
cq <825911265@qq.com>
2019-11-05 04:36:00 +0800

    商品微服务

---
+++ eshopproductha/src/main/java/com/roncoo/eshop/product/ha/Application.java
@@ -0,0 +1,15 @@
+package com.roncoo.eshop.product.ha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by Admin on 2019/11/4.
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args){
+        SpringApplication.run(Application.class,args);
+    }
+}
