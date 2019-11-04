c581a2852c3636dca0c1b655efb1525932bf561d
cq <825911265@qq.com>
2019-11-05 06:04:05 +0800

    添加商品服务

---
+++ eshopcacheha/src/main/java/com/roncoo/eshop/cache/ha/controller/CacheController.java
@@ -0,0 +1,13 @@
+package com.roncoo.eshop.cache.ha.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * 缓存服务的接口
+ * Created by Admin on 2019/11/5.
+ */
+@Controller
+@RequestMapping("/Cache")
+public class CacheController {
+}
