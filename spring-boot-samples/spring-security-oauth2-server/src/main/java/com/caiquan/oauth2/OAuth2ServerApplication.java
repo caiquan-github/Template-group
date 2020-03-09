a09f2abeeb95b8a6f6f3e8b4b2cf61d21d545d0f
caiquan-github <39827081+trusted-genius@users.noreply.github.com>
2020-03-09 20:28:24 +0800

    spring security

---
+++ spring-boot-samples/spring-security-oauth2-server/src/main/java/com/caiquan/oauth2/OAuth2ServerApplication.java
@@ -0,0 +1,28 @@
+package com.caiquan.oauth2;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+/**
+ * 认证服务器，用于获取 Token
+ * <p>
+ * Description:
+ * </p>
+ *
+ * @author Lusifer
+ * @version v1.0.0
+ * @date 2019-04-01 16:06:45
+ * @see
+ */
+@SpringBootApplication
+public class OAuth2ServerApplication {
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(OAuth2ServerApplication.class, args);
+    }
+
+
+}
