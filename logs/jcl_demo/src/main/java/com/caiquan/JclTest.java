097bdc757e5bd6a5f0a20fabcc0c9a29e8eb4eda
caiquan-github <39827081+trusted-genius@users.noreply.github.com>
2020-03-11 22:41:02 +0800

    日志门面技术 JCL

---
+++ logs/jcl_demo/src/main/java/com/caiquan/JclTest.java
@@ -0,0 +1,15 @@
+package com.caiquan;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.junit.Test;
+
+public class JclTest {
+
+    @Test
+    public void testQuick()throws Exception{
+        Log log=LogFactory.getLog(JclTest.class);
+        log.info("你好");
+
+    }
+}
