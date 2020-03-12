ef96cbb8a83fa0086a55c369881d637e32bac4bf
caiquan-github <39827081+trusted-genius@users.noreply.github.com>
2020-03-13 03:14:34 +0800

    log4j2

---
+++ logs/log4j2_demo/src/main/java/com/caiquan/Slf4jTest.java
@@ -0,0 +1,28 @@
+package com.caiquan;
+
+
+
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+public class Slf4jTest {
+
+
+    public static final Logger LOGGER = LoggerFactory.getLogger(Slf4jTest.class);
+
+
+    // 快速入门
+    @Test
+    public void test01()throws Exception{
+        // 日志输出
+        LOGGER.error("error");
+        LOGGER.warn("wring");
+        LOGGER.info("info");
+        LOGGER.debug("debug");
+        LOGGER.trace("trace");
+
+
+    }
+}
