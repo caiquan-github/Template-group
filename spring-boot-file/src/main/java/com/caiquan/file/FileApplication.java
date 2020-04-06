7959aebba950d3e8f5dd0174a8373fb04a787697
caiquan-github <39827081+trusted-genius@users.noreply.github.com>
2020-04-06 22:15:50 +0800

    spring boot 文件上传和下载

---
+++ spring-boot-file/src/main/java/com/caiquan/file/FileApplication.java
@@ -0,0 +1,17 @@
+package com.caiquan.file;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Kwon
+ * @Title:
+ * @Description:
+ * @date 2020/4/621:47
+ */
+@SpringBootApplication
+public class FileApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(FileApplication.class,args);
+    }
+}
---
+++ spring-boot-file/src/main/java/com/caiquan/file/controller/FileUploadController.java
@@ -0,0 +1,145 @@
+package com.caiquan.file.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.*;
+import java.util.List;
+
+@Controller
+public class FileUploadController {
+
+    final static String FILEPATH="/Users/ToisKwon/test";
+
+    /*
+     * 获取file.html页面
+     */
+    @RequestMapping("file")
+    public String file(){
+        return "/file";
+    }
+    
+    /**
+     * 实现文件上传
+     * */
+    @RequestMapping("fileUpload")
+    @ResponseBody
+    public String fileUpload(@RequestParam("fileName") MultipartFile file){
+        if(file.isEmpty()){
+            return "false";
+        }
+        String fileName = file.getOriginalFilename();
+        int size = (int) file.getSize();
+        System.out.println(fileName + "-->" + size);
+        
+        String path = FILEPATH;
+        File dest = new File(path + "/" + fileName);
+        if(!dest.getParentFile().exists()){ //判断文件父目录是否存在
+            dest.getParentFile().mkdir();
+        }
+        try {
+            file.transferTo(dest); //保存文件
+            return "true";
+        } catch (IllegalStateException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+            return "false";
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+            return "false";
+        }
+    }
+
+    /*
+     * 获取multifile.html页面
+     */
+    @RequestMapping("multifile")
+    public String multifile(){
+        return "/multifile";
+    }
+    
+    /**
+     * 实现多文件上传
+     * */
+    @RequestMapping(value="multifileUpload",method= RequestMethod.POST)
+    /**public @ResponseBody String multifileUpload(@RequestParam("fileName")List<MultipartFile> files) */
+    public @ResponseBody String multifileUpload(HttpServletRequest request){
+        
+        List<MultipartFile> files = ((MultipartHttpServletRequest)request).getFiles("fileName");
+        
+        if(files.isEmpty()){
+            return "false";
+        }
+
+        String path = FILEPATH;
+        
+        for(MultipartFile file:files){
+            String fileName = file.getOriginalFilename();
+            int size = (int) file.getSize();
+            System.out.println(fileName + "-->" + size);
+            
+            if(file.isEmpty()){
+                return "false";
+            }else{        
+                File dest = new File(path + "/" + fileName);
+                if(!dest.getParentFile().exists()){ //判断文件父目录是否存在
+                    dest.getParentFile().mkdir();
+                }
+                try {
+                    file.transferTo(dest);
+                }catch (Exception e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                    return "false";
+                } 
+            }
+        }
+        return "true";
+    }
+
+    @RequestMapping("/download/{filename:.+}")
+    public String downLoad(HttpServletResponse response,@PathVariable("filename") String filename) throws UnsupportedEncodingException {
+
+        String filePath = FILEPATH ;
+        File file = new File(filePath + "/" + filename);
+        if(file.exists()){ //判断文件父目录是否存在
+            response.setContentType("application/vnd.ms-excel;charset=UTF-8");
+            response.setCharacterEncoding("UTF-8");
+            // response.setContentType("application/force-download");
+            response.setHeader("Content-Disposition", "attachment;fileName=" +   java.net.URLEncoder.encode(filename,"UTF-8"));
+            byte[] buffer = new byte[1024];
+            FileInputStream fis = null; //文件输入流
+            BufferedInputStream bis = null;
+
+            OutputStream os = null; //输出流
+            try {
+                os = response.getOutputStream();
+                fis = new FileInputStream(file);
+                bis = new BufferedInputStream(fis);
+                int i = bis.read(buffer);
+                while(i != -1){
+                    os.write(buffer);
+                    i = bis.read(buffer);
+                }
+
+            } catch (Exception e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+            System.out.println("----------file download---" + filename);
+            try {
+                bis.close();
+                fis.close();
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
+        return null;
+    }
+}
