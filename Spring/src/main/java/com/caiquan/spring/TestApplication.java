6e93680bd0a8fd347b6dc60be4ada05e19d56afe
Kwon <a4220662@hotmail.com>
2020-07-23 16:33:50 +0800

    添加maven打包指定启动项

--- Spring/src/main/java/com/caiquan/spring/TestApplication.java
+++ Spring/src/main/java/com/caiquan/spring/TestApplication.java
@@ -1,17 +1,17 @@
 package com.caiquan.spring;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
  * @author Kwon
  * @Title:
  * @Description:
- * @date 2020/3/2322:11
+ * @date 2020/7/23 14:22
  */
 @SpringBootApplication
-public class SpringApplication {
+public class TestApplication {
     public static void main(String[] args) {
-        org.springframework.boot.SpringApplication.run(SpringApplication.class,args);
-
+        SpringApplication.run(TestApplication.class,args);
     }
 }
--- Spring/src/main/java/com/caiquan/spring/controler/HelloControler.java
+++ Spring/src/main/java/com/caiquan/spring/controler/HelloControler.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-
 /**
  * @author Kwon
  * @Title:
@@ -18,7 +17,6 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class HelloControler {
     @Autowired
     HelloService helloServiceImpl;
-
 
     @GetMapping("hello")
     @ResponseBody
