8f3d134beb6eb99db755ef74d04ad1814585bbef
Ku <a4220662@hotmail.com>
2020-11-15 23:02:47 +0800

    Update BasicBuffer.java

--- nio/src/main/java/com/caiquan/nio/BasicBuffer.java
+++ nio/src/main/java/com/caiquan/nio/BasicBuffer.java
@@ -6,17 +6,18 @@ public class BasicBuffer {
 
     public static void main(String[] args) {
         //创建一个buffer 大小为5
+        //Buffer 底层是一个数组
         IntBuffer intBuffer=IntBuffer.allocate(5);
-        //put 5个数据
+        //put 4个数据
         intBuffer.put(10);
         intBuffer.put(11);
         intBuffer.put(12);
         intBuffer.put(13);
-        intBuffer.put(14);
 
         //将buffer读写状态切换
         intBuffer.flip();
 
+        intBuffer.clear();
         //read 数据
         while (intBuffer.hasRemaining()){
             //每get一次相当于指针会移到下一个数据
