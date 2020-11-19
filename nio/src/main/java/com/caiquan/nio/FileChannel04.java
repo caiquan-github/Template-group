2604d1be671cdb9049793b8ba85f2c111042f25b
Kwon <a4220662@hotmail.com>
2020-11-19 19:27:09 +0800

    Update FileChannel04.java

--- nio/src/main/java/com/caiquan/nio/FileChannel04.java
+++ nio/src/main/java/com/caiquan/nio/FileChannel04.java
@@ -21,6 +21,8 @@ public class FileChannel04 {
         //将channel1拷贝到channel2中
         channel2.transferFrom(channel1,0,channel1.size());
 
+        channel1.close();
+        channel2.close();
         fileInputStream.close();
         fileOutputStream.close();
     }
