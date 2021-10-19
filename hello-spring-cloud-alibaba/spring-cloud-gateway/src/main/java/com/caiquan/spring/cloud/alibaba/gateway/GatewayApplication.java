fe81e6f7d68e21e2607f7e98f343b1aee403eeee
caiquan <825911265@qq.com>
2021-10-19 13:05:56 +0800

    Update GatewayApplication.java

--- hello-spring-cloud-alibaba/spring-cloud-gateway/src/main/java/com/caiquan/spring/cloud/alibaba/gateway/GatewayApplication.java
+++ hello-spring-cloud-alibaba/spring-cloud-gateway/src/main/java/com/caiquan/spring/cloud/alibaba/gateway/GatewayApplication.java
@@ -3,7 +3,6 @@ package com.caiquan.spring.cloud.alibaba.gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 
 /**
  * @ClassName GatewayApplication
