fb9a4fb7e67c631c768fc4d2188ec605bc54a45d
Kwon <a4220662@hotmail.com>
2022-06-28 18:45:35 +0800

    Update RetryTest.java

--- guava/src/test/java/RetryTest.java
+++ guava/src/test/java/RetryTest.java
@@ -11,11 +11,16 @@ public class RetryTest {
     public static void main(String[] args) throws ExecutionException, RetryException {
         Retryer<Boolean> retryer = RetryerBuilder.<Boolean>newBuilder()
                 .retryIfException() //发送异常则充实
-                .retryIfResult(Predicates.equalTo(false)) //对返回的结果判断，是否 重试
-                .withAttemptTimeLimiter(AttemptTimeLimiters.fixedTimeLimit(1, TimeUnit.SECONDS)) //执行时间限制策略
-                .withStopStrategy(StopStrategies.stopAfterAttempt(5)) //停止策略
+                .retryIfResult(Predicates.equalTo(false)) //对返回的结果判断，是否 重试   为false 就重试
+                .withStopStrategy(StopStrategies.stopAfterAttempt(5)) //停止策略 最多5次 然后抛出异常
                 .build();
-//Retryer调用
-        retryer.call(() -> true);
+        //Retryer调用
+        retryer.call(() -> {
+            System.out.println("执行n次");
+            if (1==1){
+                throw new RuntimeException("test");
+            }
+        return true;
+        });
     }
 }
