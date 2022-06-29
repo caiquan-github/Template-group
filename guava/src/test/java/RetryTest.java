f13af212b79e5b1ee418ff98b60062910076e48c
Kwon <a4220662@hotmail.com>
2022-06-29 09:58:08 +0800

    Update RetryTest.java

--- guava/src/test/java/RetryTest.java
+++ guava/src/test/java/RetryTest.java
@@ -9,18 +9,23 @@ import java.util.concurrent.TimeUnit;
  */
 public class RetryTest {
     public static void main(String[] args) throws ExecutionException, RetryException {
-        Retryer<Boolean> retryer = RetryerBuilder.<Boolean>newBuilder()
-                .retryIfException() //发送异常则充实
-                .retryIfResult(Predicates.equalTo(false)) //对返回的结果判断，是否 重试   为false 就重试
+        Retryer<String> retryer = RetryerBuilder.<String>newBuilder()
+                .retryIfException() //发送异常则重试
+                .retryIfResult(Predicates.equalTo("didi")) //对返回的结果判断，是否 重试   为“didi” 就重试
+                .withWaitStrategy(WaitStrategies.fixedWait(1, TimeUnit.SECONDS)) //重试间隔1秒
                 .withStopStrategy(StopStrategies.stopAfterAttempt(5)) //停止策略 最多5次 然后抛出异常
                 .build();
         //Retryer调用
-        retryer.call(() -> {
-            System.out.println("执行n次");
-            if (1==1){
-                throw new RuntimeException("test");
-            }
-        return true;
-        });
+        try {
+            String call = retryer.call(() -> {
+                System.out.println(Thread.currentThread().getName());
+                return "didi";
+            });
+        }catch (RetryException e){
+            //重试失败了 如果处理
+            System.out.println("重试失败后处理");
+        }
+
+
     }
 }
