30ba7a4389c676e7163d33ceb33522818969ca1b
Kwon <a4220662@hotmail.com>
2022-06-29 14:18:27 +0800

    add guava

---
+++ guava/src/test/java/CaseFormatTest.java
@@ -0,0 +1,14 @@
+import com.google.common.base.CaseFormat;
+
+/**
+ * @author Kwon
+ */
+public class CaseFormatTest {
+    public static void main(String[] args) {
+
+        String data = "test_data";
+        System.out.println(CaseFormat.LOWER_HYPHEN.to(CaseFormat.LOWER_CAMEL, "test-data"));
+        System.out.println(CaseFormat.LOWER_UNDERSCORE.to(CaseFormat.LOWER_CAMEL, "test_data"));
+        System.out.println(CaseFormat.UPPER_UNDERSCORE.to(CaseFormat.UPPER_CAMEL, "test_data"));
+    }
+}
