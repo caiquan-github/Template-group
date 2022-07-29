655344373b22b9a1671ac209dfbdff577fc04ea9
Kwon <a4220662@hotmail.com>
2022-07-29 10:47:53 +0800

    golang demo - buffer channel

---
+++ go-demo/goroutine/src/kwon/bufferChannel.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	ch := make(chan string, 3)
+	ch <- "naveen"
+	ch <- "paul"
+	fmt.Println(<-ch)
+	fmt.Println(<-ch)
+
+	fmt.Println("capacity is", cap(ch))
+	fmt.Println("length is", len(ch))
+	ch <- "dwdwddw"
+	fmt.Println("read value", <-ch)
+	fmt.Println("new length is", len(ch))
+}
