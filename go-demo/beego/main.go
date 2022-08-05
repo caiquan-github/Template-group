d4af66aab4b184b8c5bd0d34a158f280ab9275ef
Kwon <a4220662@hotmail.com>
2022-08-05 19:39:33 +0800

    Update main.go

--- go-demo/beego/main.go
+++ go-demo/beego/main.go
@@ -2,9 +2,23 @@ package main
 
 import (
 	_ "beego/routers"
+	"embed"
+	"fmt"
 	beego "github.com/beego/beego/v2/server/web"
+	"io/fs"
+	"io/ioutil"
 )
 
+//go:embed views
+var f embed.FS
+
 func main() {
+	data, _ := f.ReadDir("views")
+
+	as, _ := fs.Sub(f, "views")
+	hi, _ := as.Open("index.tpl")
+	data1, _ := ioutil.ReadAll(hi)
+	fmt.Println(string(data1))
+	fmt.Println(data)
 	beego.Run()
 }
