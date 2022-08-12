db1bd2a5cc3f745d63d55034cdec8e45344fa4c4
Kwon <a4220662@hotmail.com>
2022-08-12 17:17:28 +0800

    1.1.0 demo

--- python-study/base/nester/nester.py
+++ python-study/base/nester/nester.py
@@ -8,3 +8,19 @@ def print_lol(the_list):
             print_lol(each_item)
         else:
             print(each_item)
+
+
+def print_lol(the_list, level):
+    """递归打印所有list内容"""
+    for each_item in the_list:
+        if isinstance(each_item, list):
+            print_lol(each_item, level + 1)
+        else:
+            for tab_stop in range(level):
+                print("\t", end='')
+            print(each_item)
+
+
+def test():
+    movies = ["the", 1, "the 2", 2, ["2323", 2332, 5555, ["dwdw", "123123"]]]
+    print_lol(movies, 0)
