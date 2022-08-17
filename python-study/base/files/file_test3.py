ac8fdbf49be6794ca0efe946795e573d863ce006
Kwon <a4220662@hotmail.com>
2022-08-17 14:21:22 +0800

    python demo nester 1.4.0

---
+++ python-study/base/files/file_test3.py
@@ -0,0 +1,2 @@
+with open('main_data.txt') as data:
+    print(data.readline())
--- python-study/base/nester/nester.py
+++ python-study/base/nester/nester.py
@@ -1,16 +1,17 @@
 """这里nester模块，提供了 print_lol函数"""
+import sys
 
 
-def print_lol(the_list, indent=False, level=0):
+def print_lol(the_list, indent=False, level=0, fn=sys.stdout):
     """递归打印所有list内容"""
     for each_item in the_list:
         if isinstance(each_item, list):
-            print_lol(each_item, indent, level + 1)
+            print_lol(each_item, indent, level + 1, fn)
         else:
             if indent:
                 for tab_stop in range(level):
-                    print("\t", end='')
-            print(each_item)
+                    print("\t", end='', file=fn)
+            print(each_item, file=fn)
 
 
 def test():
