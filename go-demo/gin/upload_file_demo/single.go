3f062be43b8146d2ae01c649dc3b368d7e5137be
Kwon <a4220662@hotmail.com>
2022-09-17 16:57:02 +0800

    文件上传demo

---
+++ go-demo/gin/upload_file_demo/single.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"log"
+	"net/http"
+)
+
+func main() {
+	router := gin.Default()
+	// 处理multipart forms提交文件时默认的内存限制是32 MiB
+	// 可以通过下面的方式修改
+	// router.MaxMultipartMemory = 8 << 20  // 8 MiB
+	router.POST("/upload", func(c *gin.Context) {
+		// 单个文件
+		file, err := c.FormFile("f1")
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{
+				"message": err.Error(),
+			})
+			return
+		}
+
+		log.Println(file.Filename)
+		dst := fmt.Sprintf("C:/tmp/%s", file.Filename)
+		// 上传文件到指定的目录
+		c.SaveUploadedFile(file, dst)
+		c.JSON(http.StatusOK, gin.H{
+			"message": fmt.Sprintf("'%s' uploaded!", file.Filename),
+		})
+	})
+	router.Run()
+}
