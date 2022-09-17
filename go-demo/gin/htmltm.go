2fc31f9aff83f83ac5cd1c7f3e5f0ad4d08fd44d
Kwon <a4220662@hotmail.com>
2022-09-17 16:17:18 +0800

    gin html demo

---
+++ go-demo/gin/htmltm.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+func main() {
+	r := gin.Default()
+	r.LoadHTMLGlob("templates/**/*")
+	//r.LoadHTMLFiles("templates/posts/index.html", "templates/users/index.html")
+	r.GET("/posts/index", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "posts/index.html", gin.H{
+			"title": "posts/index",
+		})
+	})
+
+	r.GET("users/index", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "users/index.html", gin.H{
+			"title": "users/index",
+		})
+	})
+
+	r.Run(":8080")
+}
---
+++ go-demo/gin/restful.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+	r.GET("/book", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "GET",
+		})
+	})
+
+	r.POST("/book", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "POST",
+		})
+	})
+
+	r.PUT("/book", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "PUT",
+		})
+	})
+
+	r.DELETE("/book", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "DELETE",
+		})
+	})
+
+	// 启动HTTP服务，默认在0.0.0.0:8080启动服务
+	r.Run()
+}
