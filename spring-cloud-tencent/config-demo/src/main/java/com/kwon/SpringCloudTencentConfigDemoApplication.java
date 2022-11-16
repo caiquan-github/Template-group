233ac2da41cb067f70ab71372239398629b20b26
Kwon <a4220662@hotmail.com>
2022-11-16 14:57:04 +0800

    todo

--- spring-cloud-tencent/config-demo/src/main/java/com/kwon/SpringCloudTencentConfigDemoApplication.java
+++ spring-cloud-tencent/config-demo/src/main/java/com/kwon/SpringCloudTencentConfigDemoApplication.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
-// TODO: 2022/11/16  没有测通 拿不到配置文件 
+// TODO: 2022/11/16  没有测通 拿不到配置文件
 @SpringBootApplication
 public class SpringCloudTencentConfigDemoApplication {
 
