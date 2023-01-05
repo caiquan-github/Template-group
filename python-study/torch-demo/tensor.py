5dcb6bff9f683a634708dd1901959606de4a15a9
Kwon <a4220662@hotmail.com>
2023-01-05 10:42:25 +0800

    torch demo

---
+++ python-study/torch-demo/tensor.py
@@ -0,0 +1,58 @@
+from __future__ import print_function
+import torch
+
+x = torch.empty(5, 3)
+print(x)
+print('--------------------------------------')
+
+x = torch.rand(5, 3)
+print(x)
+print('--------------------------------------')
+
+x = torch.zeros(5, 3, dtype=torch.long)
+print(x)
+print('--------------------------------------')
+
+x = torch.tensor([5.5, 3])
+print(x)
+print('--------------------------------------')
+
+# 或根据现有的 tensor 建立新的 tensor 。除非用户提供新的值，否则这些方法将重用输入张量的属性，例如 dtype 等：
+x = x.new_ones(5, 3, dtype=torch.double)  # new_* methods take in sizes
+print(x)
+x = torch.randn_like(x, dtype=torch.float)  # 重载 dtype!
+print(x)  # 结果size一致
+print(x.size())  # 获取张量的状态
+print('--------------------------------------')
+
+# 加法
+y = torch.rand(5, 3)
+print(x + y)
+# 指定输出
+result = torch.empty(5, 3)
+torch.add(x, y, out=result)
+print(result)
+print('--------------------------------------')
+
+# adds x to y
+print(y)
+print(x)
+y.add_(x)  # 注意：任何一个就地改变张量的操作后面都固定一个 _ 。例如 x.copy_（y）， x.t_（）将更改x
+print(y)
+print('--------------------------------------')
+
+# 也可以使用像标准的 NumPy 一样的各种索引操作：
+print(x[:, 1])
+print('--------------------------------------')
+
+# 改变形状：如果想改变形状，可以使用 torch.view
+x = torch.randn(4, 4)
+y = x.view(16)  # 这里是把上面的二维数组转换成了一维数组
+z = x.view(-1, 8)  # 这个-1 代表根据实际长度会推断出来  比如这里16/8=2
+print(x, y, z, sep='\n')
+print(x.size(), y.size(), z.size())
+print('--------------------------------------')
+
+x = torch.randn(1)
+print(x)
+print(x.item())  # 如果是仅包含一个元素的 tensor，可以使用 .item（） 来得到对应的 python 数值
---
+++ python-study/torch-demo/tensor_numpy.py
@@ -0,0 +1,45 @@
+from __future__ import print_function
+import torch
+import numpy as np
+
+# 桥接NumPy
+# 将一个 Torch 张量转换为一个 NumPy 数组是轻而易举的事情，反之亦然。
+# Torch 张量和 NumPy数组将共享它们的底层内存位置，因此当一个改变时，另外也会改变。
+
+a = torch.ones(5)
+print(a)
+
+b = a.numpy()
+print(b)
+
+a.add_(1)
+print(a)
+print(b)
+
+print('------------------------------')
+
+# 将 NumPy 数组转化为Torch张量
+# 看改变 NumPy 分配是如何自动改变 Torch 张量的：
+a = np.ones(5)
+b = torch.from_numpy(a)
+print(a)
+print(b)
+np.add(a, 1, out=a)
+print(a)
+print(b)
+
+print('------------------------------')
+
+# CUDA上的张量
+
+
+# 当GPU可用时,我们可以运行以下代码
+# 我们将使用`torch.device`来将tensor移入和移出GPU
+print(torch.cuda.is_available())
+# if torch.cuda.is_available():
+#     device = torch.device("cuda")  # a CUDA device object
+#     y = torch.ones_like(x, device=device)  # 直接在GPU上创建tensor
+#     x = x.to(device)  # 或者使用`.to("cuda")`方法
+#     z = x + y
+#     print(z)
+#     print(z.to("cpu", torch.double))  # `.to`也能在移动时改变dtype
