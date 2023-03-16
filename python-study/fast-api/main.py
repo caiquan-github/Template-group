13a4045ac6f7edf023d331e577250507c263ce9e
Kwon <a4220662@hotmail.com>
2023-03-16 15:32:09 +0800

    Update main.py

--- python-study/fast-api/main.py
+++ python-study/fast-api/main.py
@@ -1,16 +1,16 @@
 from typing import Union
 from fastapi import FastAPI
 
-
-
 app = FastAPI()
 
 
+# http://127.0.0.1:8000
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
 
 
+# http://127.0.0.1:8000/items/213?q=dwdw
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
