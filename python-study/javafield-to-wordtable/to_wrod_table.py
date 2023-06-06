657693788f256a65890d4748ea3e56335f0677b4
Kwon <a4220662@hotmail.com>
2023-06-06 17:20:52 +0800

    Update to_wrod_table.py

--- python-study/javafield-to-wordtable/to_wrod_table.py
+++ python-study/javafield-to-wordtable/to_wrod_table.py
@@ -7,16 +7,24 @@ strs = []
 
 new = open('out.txt', encoding='utf8', mode='w')
 
+st = ''
+
 index = 1
 for i in file:
     i = i.strip()
     if not i.startswith('private'):
+        if i.startswith('//'):
+            st += i.replace('//', '')
         continue
 
     # 根据空格分割  0舍弃，1类型，2参数，3注解 可能没有注释
     i = re.split('[; ]', i)
     i3 = "".join(i[3: len(i) + 1])
     i3 = i3.replace('//', '')
-    put = str(index) + '\t' + i[2] + '\t' + '0' + ' \t' + i[1] + '\t' + i3 + '\t' + i3 + '\n'
+
+    if i3 == '' and st != '':
+        i3 = st
+        st = ''
+    put = str(index) + '\t' + i[2] + '\t' + '0' + ' \t' + i[1] + '\t' + i3 + '\n'
     new.writelines(put)
     index = index + 1
