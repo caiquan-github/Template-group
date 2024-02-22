af8fc825505a1b0d14100933af9c9d6eb006f2e1
Kwon <a4220662@hotmail.com>
2024-02-22 17:05:50 +0800

    exception demo

---
+++ jdk-exception-demo/ExplicableErrorCode.java
@@ -0,0 +1,24 @@
+
+/**
+ * 错误码
+ * @author Kwon
+ * @date 2024/2/22 16:56
+ */
+public interface ExplicableErrorCode {
+    /**
+     * 返回错误信息
+     *
+     * @return
+     * @author Kwon
+     * @date 2024/2/22 16:57
+     */
+    String getMsg();
+    /**
+     * 返回错误码
+     *
+     * @return
+     * @author Kwon
+     * @date 2024/2/22 16:57
+     */
+    String getCode();
+}
---
+++ jdk-exception-demo/LoanManageErrorCode.java
@@ -0,0 +1,24 @@
+// 贷款管理错误代码
+public enum LoanManageErrorCode implements ExplicableErrorCode{
+    REPAY_PRINCIPAL_IS_GREATER_THAN_PRINCIPAL("repay principal (%s) is greater than principal (%s)"),
+
+    REPAY_PRINCIPAL_IS_NEGATIVE("repay principal (%s) is negative")
+    ;
+
+
+    LoanManageErrorCode(String msg){
+       this.msg=msg;
+    }
+
+    String msg;
+
+    @Override
+    public String getMsg() {
+        return msg;
+    }
+
+    @Override
+    public String getCode() {
+        return this.name();
+    }
+}
