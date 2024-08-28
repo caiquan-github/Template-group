fa21fdc5e0af20e1964c75a70dfb5c826de53ca4
caiquan <825911265@qq.com>
2024-08-28 10:57:16 +0800

    Update EventBusTest.java

--- guava/src/test/java/EventBusTest.java
+++ guava/src/test/java/EventBusTest.java
@@ -1,16 +1,20 @@
+import com.google.common.eventbus.AsyncEventBus;
 import com.google.common.eventbus.EventBus;
 import com.google.common.eventbus.Subscribe;
 import org.junit.Test;
+
+import java.util.concurrent.Executors;
 
 /**
  * @author Kwon
  */
 public class EventBusTest {
     public static void main(String[] args) {
-        EventBus eventBus = new EventBus("lwl");
+        EventBus eventBus = new AsyncEventBus(Executors.newCachedThreadPool());
         eventBus.register(new OrderEventListener());
+        eventBus.register(new OrderEventListener2());
         eventBus.post(new OrderMessage("csc"));
-
+        System.out.println("完成");
     }
 }
 
@@ -35,5 +39,12 @@ class OrderEventListener {
     @Subscribe
     public void dealWithEvent(OrderMessage event) {
         System.out.println("内容：" + event.getMessage());
+    }
+}
+
+class OrderEventListener2 {
+    @Subscribe
+    public void dealWithEvent(OrderMessage event) {
+        System.out.println("内容2：" + event.getMessage());
     }
 }
